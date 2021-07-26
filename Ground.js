d6d27c0189f815155c4b4c6034616e635b4af2d4
varundahake <77110699+varundahake@users.noreply.github.com>
2021-07-26 17:39:30 +0530

    Add files via upload

--- Ground.js
+++ Ground.js
@@ -9,7 +9,7 @@ class Ground {
       this.body = Bodies.rectangle(x,y,width,height,options);
       this.width = width;
       this.height = height;
-      World.add(world, this.body);
+      World.add(world,this.body);
     }
     display(){
       
